85a6f0a4c83b3fc8cdb2ebefadb8ab9f37013f5d
Никита <kadyshevnik@yandex.ru>
2025-03-06 23:17:19 +0500

    chenge box

--- client.py
+++ client.py
@@ -2,11 +2,12 @@ import pygame
 import socket
 import json
 import threading
+import ctypes
 
 HOST = '127.0.0.1'
 PORT = 65432
 
-WIDTH, HEIGHT = 500, 500
+WIDTH, HEIGHT = 800, 800  # Увеличенное поле
 GRID_SIZE = 20
 WALL_THICKNESS = 5
 
@@ -23,6 +24,12 @@ class SnakeGame:
         self.direction = (GRID_SIZE, 0)
         self.running = True
         self.server_state = {}
+        self.move_delay = 300  # Задержка движения в мс
+        self.last_move_time = pygame.time.get_ticks()
+        
+        # Устанавливаем окно поверх всех окон
+        hwnd = pygame.display.get_wm_info()["window"]
+        ctypes.windll.user32.SetWindowPos(hwnd, -1, 0, 0, 0, 0, 3)
 
     def connect(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -43,10 +50,14 @@ class SnakeGame:
 
     def handle_keys(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]: self.direction = (0, -GRID_SIZE)
-        if keys[pygame.K_DOWN]: self.direction = (0, GRID_SIZE)
-        if keys[pygame.K_LEFT]: self.direction = (-GRID_SIZE, 0)
-        if keys[pygame.K_RIGHT]: self.direction = (GRID_SIZE, 0)
+        if keys[pygame.K_UP] and self.direction != (0, GRID_SIZE):
+            self.direction = (0, -GRID_SIZE)
+        if keys[pygame.K_DOWN] and self.direction != (0, -GRID_SIZE):
+            self.direction = (0, GRID_SIZE)
+        if keys[pygame.K_LEFT] and self.direction != (GRID_SIZE, 0):
+            self.direction = (-GRID_SIZE, 0)
+        if keys[pygame.K_RIGHT] and self.direction != (-GRID_SIZE, 0):
+            self.direction = (GRID_SIZE, 0)
 
     def check_collision(self):
         head = self.snake[0]
@@ -54,11 +65,14 @@ class SnakeGame:
             self.running = False
 
     def update(self):
-        head = (self.snake[0][0] + self.direction[0], self.snake[0][1] + self.direction[1])
-        self.snake.insert(0, head)
-        self.snake.pop()
-        self.check_collision()
-        self.send_data()
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_move_time >= self.move_delay:
+            self.last_move_time = current_time
+            head = (self.snake[0][0] + self.direction[0], self.snake[0][1] + self.direction[1])
+            self.snake.insert(0, head)
+            self.snake.pop()
+            self.check_collision()
+            self.send_data()
 
     def draw(self):
         self.screen.fill(COLORS["background"])
@@ -81,7 +95,7 @@ class SnakeGame:
             self.handle_keys()
             self.update()
             self.draw()
-            self.clock.tick(10)
+            self.clock.tick(60)  # Высокая частота обновления для отзывчивости
         self.sock.close()
         pygame.quit()
 
